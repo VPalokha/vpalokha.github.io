1d75da560c68942ac98ba60056d0d7755429997a
Виталий <vitos1306@gmail.com>
2020-11-04 12:11:51 +0200

    bug fix

--- cards/js/forms/FormDentist.js
+++ cards/js/forms/FormDentist.js
@@ -3,9 +3,10 @@ import Form from "./Form.js"
 import {fieldsForm} from "./FieldsForm.js"
 
 export default class FormDentist extends Form {
-   constructor(doctor) {
-      super(doctor)
+   constructor(doctor,purpose, desc, priority, age, id,pressure, weightIndex, heartIllness, fullName, lastDateVisit) {
+      super(doctor, fullName, priority, purpose, desc)
       this.lastDateVisit = new Input(fieldsForm.lastDateVisit, "form__input").create();
+      this.placeholderForEdit(lastDateVisit, this.lastDateVisit, 'Измените/подтвердите дату последнего визита:' )
    }
 
    render(modal) {

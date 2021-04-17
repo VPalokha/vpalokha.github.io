050ae43d175b60d4291ee5731d85aabc0ce6ecf5
Виталий <vitos1306@outlook.com>
2021-04-17 19:18:45 +0300

    hair

--- hair/js/index.js
+++ hair/js/index.js
@@ -1,15 +1,22 @@
 const social = document.getElementById('mySocial');
 const btn = document.getElementById("myBtn");
-
-
 btn.onclick = function () {
   social.style.display = "block";
   btn.style.display = "none"
 }
-
 window.onclick = function (event) {
   if (event.target === social) {
     social.style.display = "none";
     btn.style.display = "inline"
   }
 }
+
+
+$(".page-sections-link").on("click", function (e) {
+  e.preventDefault();
+  const href = $(this).attr("href");
+  const offset = $(href).offset().top;
+  $("html, body").animate({
+    scrollTop: offset,
+  }, 3000);
+});

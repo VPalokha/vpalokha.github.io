010ea38a22ba3de297124864dc04fe09fa3b7c24
Виталий <vitos1306@outlook.com>
2021-04-18 19:18:58 +0300

    hair

--- hair/js/index.js
+++ hair/js/index.js
@@ -11,6 +11,9 @@ window.onclick = function (event) {
   }
 }
 
+//
+//
+//
 
 $(".page-sections-link").on("click", function (e) {
   e.preventDefault();
@@ -19,4 +22,37 @@ $(".page-sections-link").on("click", function (e) {
   $("html, body").animate({
     scrollTop: offset,
   }, 3000);
+});
+
+//
+//
+//
+
+$('.slick').slick({
+  dots: true,
+  infinite: true,
+  touchThreshold: 100,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  centerMode: true,
+  autoplay: true,
+  autoplaySpeed: 2500,
+  arrows: false,
+  pauseOnHover: true,
+  responsive: [{
+    breakpoint: 1024,
+    settings: {
+      slidesToShow: 3,
+      slidesToScroll: 1,
+    }
+  },
+    {
+      breakpoint: 559,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
 });
